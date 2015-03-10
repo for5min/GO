697fa299018f9db9e5341aab4bceb78793642bf2
Lei Tu <alex.lei.tu@gmail.com>
2015-03-10 16:02:36 +0800

    add json

--- scheduler/scheduler.sh
+++ scheduler/scheduler.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 PROG=/home/scaleworks/scheduler
 CONF=/home/scaleworks/scheduler.json
-
+LOGFILE=/home/scaleworks/${STAGING}/current/log/scheduler.log
 PIDFILE=/var/tmp/scheduler.pid
-PID=`cat /var/tmp/scheduler`
+PID=`cat /var/tmp/scheduler.pid`
 
 case $1 in
   start)
-    $PROG $CONF 2>&1
+    $PROG $CONF >> $LOGFILE 2>&1
     echo $! > $PIDFILE
     ;;
   stop)
     kill -9 $PID
     ;;
-  restart)
-    stop
-    start
+  *) echo "start|stop"
     ;;
-  *) echo "start|stop|restart"
-    ;;
-  esac
+esac
